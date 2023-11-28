28167a6c5ccf8aaa2b44c98896ea8055a054d03e
jay <jay.mm.message@gmail.com>
2023-11-28 15:34:33 +0800

    Add a new comment

--- quick_review/get_started/myfirstprog.cpp
+++ quick_review/get_started/myfirstprog.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
     // This is a comment
     cout << "My first c plus plus program. \n";
+
+    /* The code below will print words Learning c plus plus
+    to the screen, and it is amazing */
     cout << "Learning c plus plus.";
 
     cout << endl;
