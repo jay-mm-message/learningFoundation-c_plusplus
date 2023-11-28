55768923a87582d440c02756ddb913213a17d4db
jay <jay.mm.message@gmail.com>
2023-11-28 15:12:10 +0800

    The insert a new line.

--- quick_review/get_started/myfirstprog.cpp
+++ quick_review/get_started/myfirstprog.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "My first c plus plus program.";
+    cout << "My first c plus plus program. \n";
     cout << "Learning c plus plus.";
     return 0;
 }
