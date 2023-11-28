1f69dc38d3d24635e23a6a31d2211924a04eba2e
jay <jay.mm.message@gmail.com>
2023-11-28 17:05:18 +0800

    declare a constant variable

--- quick_review/get_started/myfirstprog.cpp
+++ quick_review/get_started/myfirstprog.cpp
@@ -25,6 +25,14 @@ int main() {
     x = y = z = 50;
     cout << "x = " << x << ", y = " << y << ", z = " << z << endl;
 
+    /* note: variable 'xx' declared const here
+    const int xx = 100;
+    ~~~~~~~~~~^~~~~~~~ 1 error generated.*/
+    const int xx = 100;
+    // xx = 50;
+
+    cout << "xx = " << xx << endl;
+
     cout << endl;
     return 0;
 }
