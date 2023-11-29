a9e8052c879ac7d6c6e81cac11ca65f37a7c13e6
jay <jay.mm.message@gmail.com>
2023-11-29 16:49:50 +0800

    An example of pass arrays to function

--- quick_review/get_started/funcParam.cpp
+++ quick_review/get_started/funcParam.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void myFunction(string name = "bystander");
 int max(int a, int b);
 void swapNums(int &a, int &b); // called by reference
+void show_array(int arr[], int size);
 
 int main(int argc, char const *argv[])
 {
@@ -23,6 +24,9 @@ int main(int argc, char const *argv[])
     swapNums(a, b);
     cout << "a: " << a << ", b: " << b << endl;
 
+    int arrNums[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    show_array(arrNums, sizeof(arrNums)/sizeof(int));
+
     cout << endl;
     return 0;
 }
@@ -40,4 +44,13 @@ void swapNums(int &a, int &b) {
     int tmp = a;
     a = b;
     b = tmp;
+}
+
+void show_array(int arr[], int size) {
+
+    for (size_t i = 0; i < size; i++) {
+        /* code */
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
 }
