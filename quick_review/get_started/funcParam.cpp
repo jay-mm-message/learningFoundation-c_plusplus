95e2c4f27e7b149b6e6b0b271f4d85a756119214
jay <jay.mm.message@gmail.com>
2023-11-29 16:32:46 +0800

    An example of a function with two paramters that return the maximum values

--- quick_review/get_started/funcParam.cpp
+++ quick_review/get_started/funcParam.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 // Function declaration
 void myFunction(string name = "bystander");
+int max(int a, int b);
 
 int main(int argc, char const *argv[])
 {
@@ -11,6 +12,10 @@ int main(int argc, char const *argv[])
     myFunction("Ellen");
     myFunction("Jhon");
     myFunction();
+
+    int a = 300;
+    int b = 200;
+    cout << "(" << a << ", " << b << "), max is " << max(a, b) << endl;
     
     cout << endl;
     return 0;
@@ -19,4 +24,8 @@ int main(int argc, char const *argv[])
 // Function definition
 void myFunction(string name) {
     cout << "Hi, " << name << endl;
+}
+
+int max(int a, int b) {
+    return (a<b)?b:a;
 }
