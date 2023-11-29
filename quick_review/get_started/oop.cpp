aeae2cd8f52fc719f660c7f64a7146086f421792
jay <jay.mm.message@gmail.com>
2023-11-29 21:20:45 +0800

    Define function into MyClass, name called it myObjShow

--- quick_review/get_started/oop.cpp
+++ quick_review/get_started/oop.cpp
@@ -6,6 +6,10 @@ class MyClass {
     public:
         int myNum;
         string myString;
+    public:
+        void myObjShow() {
+            cout << "student: " << myNum << ", name: " << myString << endl;
+        }
 };
 
 int main(int argc, char const *argv[])
@@ -14,12 +18,14 @@ int main(int argc, char const *argv[])
     MyClass obj1, obj2;
     obj1.myNum = 1;
     obj1.myString = "Jay";
+    obj1.myObjShow();
     
     obj2.myNum = 2;
     obj2.myString = "Ellen";
+    obj2.myObjShow();
 
-    cout << "student: " << obj1.myNum << ", name: " << obj1.myString << endl;
-    cout << "student: " << obj2.myNum << ", name: " << obj2.myString << endl;
-    
+    //cout << "student: " << obj1.myNum << ", name: " << obj1.myString << endl;
+    //cout << "student: " << obj2.myNum << ", name: " << obj2.myString << endl;
+
     return 0;
 }
