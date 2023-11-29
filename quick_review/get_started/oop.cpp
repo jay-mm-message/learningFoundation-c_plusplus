96ab9f7d8f305ec2b49364f927c5de4fbe7d4109
jay <jay.mm.message@gmail.com>
2023-11-29 21:13:23 +0800

    Create an object of myClass

--- quick_review/get_started/oop.cpp
+++ quick_review/get_started/oop.cpp
@@ -11,5 +11,15 @@ class MyClass {
 int main(int argc, char const *argv[])
 {
     /* code */
+    MyClass obj1, obj2;
+    obj1.myNum = 1;
+    obj1.myString = "Jay";
+    
+    obj2.myNum = 2;
+    obj2.myString = "Ellen";
+
+    cout << "student: " << obj1.myNum << ", name: " << obj1.myString << endl;
+    cout << "student: " << obj2.myNum << ", name: " << obj2.myString << endl;
+    
     return 0;
 }
