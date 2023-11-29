35351f141f0d16dec19c540790db596ee3d06235
jay <jay.mm.message@gmail.com>
2023-11-29 21:10:07 +0800

    Create a class called MyClass

--- quick_review/get_started/oop.cpp
+++ quick_review/get_started/oop.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// Create a class called MyClass
+class MyClass {
+    public:
+        int myNum;
+        string myString;
+};
+
 int main(int argc, char const *argv[])
 {
     /* code */
